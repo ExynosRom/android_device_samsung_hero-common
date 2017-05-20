a336f58abf8d438bd6fa1710be965a45378243d4
deedwar <deed821@gmail.com>
2017-05-20 15:15:05 +1000

    camera: retest the wrapper using the older one
    Change-Id: If51d4d1d2d0a667d73e358db57b771576a32119a

--- product/camera.mk
+++ product/camera.mk
@@ -1,3 +1,7 @@
+PRODUCT_PACKAGES += \
+	camera.exynos5 \
+  	Snap
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
